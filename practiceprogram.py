18129ffcce211af801f81a14f76a192672830f43
Gunasekar <111216484+Gunasekar13@users.noreply.github.com>
2022-10-10 16:02:29 +0530

    Delete practiceprogram.py

--- practiceprogram.py
+++
@@ -1,7 +0,0 @@
-arr=list(map(int,input('Enter array element: ').strip().split()))
-i=int(input('Enter starting index: '))
-j=int(input('Enter ending index: '))
-sum=0
-for idx in range(i,j+1):
-	sum+=arr[idx]
-print('Output :',sum)
